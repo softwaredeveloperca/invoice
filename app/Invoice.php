42fc1e7d077da52f007314849d3579e9678f5f2b
rob d <rob@phpdeveloper.ca>
2019-06-14 16:40:20 +0000

    estimate change, added api resource & model setup changes

--- app/Invoice.php
+++ app/Invoice.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Invoice extends Model
 {
-    //
+    public function purchase_line_items()
+    {
+        return $this->hasMany('App\PurchaseLineItems', 'invoice_id');
+    }
+	
+	public function payment_line_items()
+    {
+        return $this->hasMany('App\PaymentLineItems', 'invoice_id');
+    }
+	
+	public function store()
+    {
+        return $this->hasOne('App\Store', 'id', 'store_id');
+    }
 }
--- app/PurchaseLineItems.php
+++ app/PurchaseLineItems.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class PurchaseLineItems extends Model
 {
-    //
+    public function product()
+    {
+        return $this->hasOne('App\Product', 'id', 'product_id');
+    }
 }
